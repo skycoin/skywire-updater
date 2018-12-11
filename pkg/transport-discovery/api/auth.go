5cc6e76f2915d55f4eefe7eb303b40e8b10a1b26
Gustvo Chain <gchain@gmail.com>
2018-12-11 14:56:46 +0100

    api: use TeeReader when auth reads .Body

--- pkg/transport-discovery/api/auth.go
+++ pkg/transport-discovery/api/auth.go
@@ -74,46 +74,19 @@ func (api *API) VerifyAuth(r *http.Request, auth *Auth) error {
 		return errors.New("SW-Nonce does not match")
 	}
 
-	body, err := peekBody(r)
-	if err != nil {
-		return err
-	}
+	var buf bytes.Buffer
+	body := io.TeeReader(r.Body, &buf)
 
 	payload, err := ioutil.ReadAll(body)
 	if err != nil {
 		return err
 	}
 
+	// close the original body cause it will be replaced
+	if err := r.Body.Close(); err != nil {
+		return err
+	}
+
+	r.Body = ioutil.NopCloser(&buf)
 	return auth.Verify(payload)
-}
-
-// peekBody reads the body from http.Request without removing the data from the original body.
-func peekBody(r *http.Request) (io.ReadCloser, error) {
-	var err error
-	save := r.Body
-	save, r.Body, err = drainBody(r.Body)
-
-	return save, err
-}
-
-// drainBody reads all of b to memory and then returns two equivalent
-// ReadClosers yielding the same bytes.
-
-// It returns an error if the initial slurp of all bytes fails. It does not attempt
-// to make the returned ReadClosers have identical error-matching behavior.
-func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
-	if b == http.NoBody {
-		// No copying needed. Preserve the magic sentinel meaning of NoBody.
-		return http.NoBody, http.NoBody, nil
-	}
-	var buf bytes.Buffer
-	if _, err = buf.ReadFrom(b); err != nil {
-		return nil, b, err
-	}
-
-	if err = b.Close(); err != nil {
-		return nil, b, err
-	}
-
-	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }
